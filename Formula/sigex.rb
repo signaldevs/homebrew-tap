77bb10ba2151e3292c6773be9f2c920afc2c270b
Kevin O'Hara <kevinohara80@gmail.com>
2024-10-17 16:07:39 -0400

    fix lint issues

--- Formula/sigex.rb
+++ Formula/sigex.rb
@@ -1,5 +1,5 @@
 class Sigex < Formula
-  desc "Sigex is a process runner/executor with support for multiple .env file configuration as well as automatic retrieval of secrets from supported secrets manager platforms."
+  desc "A process runner/executor with support for multiple env files"
   homepage "https://github.com/signaldevs/sigex"
   version "0.0.1"
   license "GNU Affero General Public License v3.0"
@@ -7,20 +7,20 @@ class Sigex < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/signaldevs/sigex/releases/download/v#{version}/sigex-darwin-arm64.tar.gz"
-      sha256 "6dfcbeaed1570a97b9ca85ec25149352867af741683c5b3de9e136e860d07192"  # Replace this with the actual SHA-256 sum from the workflow
+      sha256 "6dfcbeaed1570a97b9ca85ec25149352867af741683c5b3de9e136e860d07192"
     else
       url "https://github.com/signaldevs/sigex/releases/download/v#{version}/sigex-darwin-amd64.tar.gz"
-      sha256 "2839d57a0e20c82d1a57778976f5bc671ff481339d8f7fe565f548ecd60c9b7d"  # Replace this with the actual SHA-256 sum from the workflow
+      sha256 "2839d57a0e20c82d1a57778976f5bc671ff481339d8f7fe565f548ecd60c9b7d"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/signaldevs/sigex/releases/download/v#{version}/sigex-linux-arm64.tar.gz"
-      sha256 "1804d2bdc9088d5c68719e09d9ae8da8f1a3aee709fcaa039c5f3f41e910743a"  # Replace this with the actual SHA-256 sum from the workflow
+      sha256 "1804d2bdc9088d5c68719e09d9ae8da8f1a3aee709fcaa039c5f3f41e910743a"
     else
       url "https://github.com/signaldevs/sigex/releases/download/v#{version}/sigex-linux-amd64.tar.gz"
-      sha256 "2a8ef8bae55b6448fde4e34d5006781af73beb277d9b4cb5ea3ac54c6431576a"  # Replace this with the actual SHA-256 sum from the workflow
+      sha256 "2a8ef8bae55b6448fde4e34d5006781af73beb277d9b4cb5ea3ac54c6431576a"
     end
   end
 
@@ -29,6 +29,6 @@ class Sigex < Formula
   end
 
   test do
-    assert_match "sigex --version", shell_output("#{bin}/sigex --version")
+    assert_match "#{version}", shell_output("#{bin}/sigex --version")
   end
 end
